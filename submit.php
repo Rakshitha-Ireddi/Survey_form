b1317feabe910e6787a9607bc42a78120efa1d8c
Rakshitha-Ireddi <139454114+Rakshitha-Ireddi@users.noreply.github.com>
2023-08-13 14:23:04 +0530

    Add files via upload
    Use Xampp Server and execute PHP to understand the  working of SURVEY FORM

---
+++ submit.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $age = $_POST["age"];
+  $feedback = $_POST["feedback"];
+  
+  // Store the data in a database or send it via email
+  // Here, we'll just display the submitted data
+  echo "Name: $name<br>";
+  echo "Age: $age<br>";
+  echo "Feedback: $feedback<br>";
+}
+?>
