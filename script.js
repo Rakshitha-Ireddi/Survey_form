e8ee58b0319e4e61d67a371f5205d35d753aefc2
Rakshitha-Ireddi <139454114+Rakshitha-Ireddi@users.noreply.github.com>
2023-08-30 13:26:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentPage = 1;
+
+function showPage(pageNumber) {
+  const pages = document.querySelectorAll('.page');
+  pages.forEach(page => page.style.display = 'none');
+  pages[pageNumber - 1].style.display = 'block';
+  currentPage = pageNumber;
+}
+
+function nextPage() {
+  if (currentPage < 3) {
+    showPage(currentPage + 1);
+  }
+}
+
+function prevPage() {
+  if (currentPage > 1) {
+    showPage(currentPage - 1);
+  }
+}
+
+showPage(1);
